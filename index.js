7cbbe1ce070b47a7f8de70436cf880ea1c923510
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-14 16:57:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import process from "node:process";
 const server = createServer(async (req, res) => {
   let message 
   let response = await fetch("https://api.openfront.io/player/wPHaVYX4");
-  message = JSON.stringify(response.json())
-  res.end(message);
+  message = await response.json()
+  res.end(JSON.stringify(message));
 });
 
 server.listen(8080);
