7d30d1e184067ee2e88c07690438852025f2d83f
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-14 16:53:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import { createServer } from "node:http";
 import process from "node:process";
 
 const server = createServer((req, res) => {
-  const message = `Hello from ${process.env.DENO_REGION} at ${new Date()}`;
+  let message 
+  fetch("https://api.openfront.io/player/wPHaVYX4")
+  .then(res => res.json())
+  .then((json) => message = JSON.stringify(json))
+  .catch((e) => message = e);
   res.end(message);
 });
 
