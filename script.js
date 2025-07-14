2a1a1f583992261f72d414f3eb8baea2a8c609d8
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-14 17:06:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://corsproxy.io/?https://api.openfront.io/player/wPHaVYX4")
+fetch("https://tktk123456-openfrontio-51.deno.dev/")
   .then(res => res.json())
   .then((json) => alert(JSON.stringify(json)))
   .catch(alert);
