eaef171b181d42f1d734a8f8be58914605a7a0e5
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-14 18:48:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import { createServer } from "node:http";
 
 const server = createServer(async (req, res) => {
+  
   try {
-    //const response = await fetch("https://api.openfront.io/player/wPHaVYX4");
-    const response = await fetch("https://api.openfront.io/game/d9oFrfjL")
+    const response = await fetch(`https://api.openfront.io/player/wPHaVYX4`);
+    //const response = await fetch(`https://api.openfront.io/game/d9oFrfjL`)
     // Forward status code & content-type
     res.statusCode = response.status;
     res.setHeader("Content-Type", response.headers.get("Content-Type") || "application/json");
     res.setHeader("Access-Control-Allow-Origin", "*"); // Enable CORS for clients
 
     const data = await response.text(); // Use text() to forward raw data
-    res.end(Object.getOwnPropertyNames(data).join("\n"));
+    res.end(data);
   } catch (e) {
     res.statusCode = 500;
     res.setHeader("Content-Type", "text/plain");
