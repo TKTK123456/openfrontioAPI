6d919ce12e92192742ad1253fecf5679b06da4f2
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-15 07:52:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function fetchInfo(id, from = "player") {
   if (from==="player") return fetch(`https://tktk123456-openfrontio-51.deno.dev/player?id=${id}`).then(res => res.json()).then((json) => return json).catch((e) => return e);
 }
+document.getElementById("outputJson").textContent = fetchInfo("wPHaVYX4", "player")
