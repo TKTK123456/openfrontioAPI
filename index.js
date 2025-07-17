d6db679de865905eaddcf301faa1f9f4fca31401
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-17 09:57:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("/game", async (req, res) => {
     const data = await response.text();
     if (response.status === 200) {
       let currentIDs = await kv.get(["games", "ids"])
-      currentIds = currentIDs.values
+      currentIDs = currentIDs.values
       console.log(currentIDs)
       kv.set(["games", "ids"], )
     }
