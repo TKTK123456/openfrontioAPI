3e79648661eecfa3fb627dc9c62e383a52d023ff
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-17 11:15:24 -0400

    Update info.js

--- info.js
+++ info.js
@@ -1,4 +1,13 @@
 const kv = await Deno.openKv();
+export async function addToKvSet(key, value) {
+  let allVals = await kv.get(key)
+  allVals = allVals.value
+  if (!allVals) {
+    allVals = new Set()
+  }
+  allVals.add(value)
+  kv.set(key, allVals)
+}
 async function updateGameInfo() {
   
 }
