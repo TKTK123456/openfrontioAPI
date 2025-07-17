962457d50616ea4bc9858e195c208941f17101b8
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-17 12:15:54 -0400

    Update info.js

--- info.js
+++ info.js
@@ -2,24 +2,39 @@ import { findGameWebSocket, findPublicLobbyWebSocket, getPlayer, getGame } from 
 const kv = await Deno.openKv();
 export const setHelpers = {
   add: async function(key, value) {
-    let allVals = await kv.get(key);
-    allVals = allVals.value;
-    if (!allVals) {
-      allVals = new Set();
-    }
-    allVals.add(value);
-    kv.set(key, allVals);
+    let fullSet = await this.getSet(key)
+    fullSet.add(value);
+    kv.set(key, fullSet);
   },
-  get: async function(key) {
+  getSet: async function(key) {
     let output = await kv.get(key);
+    if (!output) {
+      output = new Set();
+    }
+    return output.value;
+  }
+}
+export const mapHelpers = {
+  set: async function(key, mapKey, value) {
+    let fullMap = await this.getMap(key)
+    fullMap.set(mapKey, value)
+    kv.set(key, fullMap)
+  },
+  getMap: async function(key) {
+    let output = await kv.get(key);
+    if (!output) {
+      output = new Map();
+    }
     return output.value;
   }
 }
 async function updateGameInfo() {
   await findPublicLobbyWebSocket()
   let active = {
-    ids: (async () => {let out = await setHelpers.get(["info", "games", "active", "ids"]);return out;})(),
-    ws: (async () => {let out = await setHelpers.get(["info", "games", "active", "ws"]);return out;})()
+    ids: (async () => {let out = await setHelpers.getSet(["info", "games", "active", "ids"]);return out;})(),
+    ws: (async () => {let out = await setHelpers.getSet(["info", "games", "active", "ws"]);return out;})()
+  }
+  for (let i = 0;i<active.ids.length;i++) {
+    
   }
 }
-updateGameInfo()
