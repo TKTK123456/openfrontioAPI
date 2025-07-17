cf1d700e08da3f81ed83ff59250a37b116825f8a
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-17 11:25:11 -0400

    Update info.js

--- info.js
+++ info.js
@@ -8,6 +8,20 @@ export async function addToKvSet(key, value) {
   allVals.add(value)
   kv.set(key, allVals)
 }
+export const sets = {
+  add: async function(key, value) {
+    let allVals = await kv.get(key)
+    allVals = allVals.value
+    if (!allVals) {
+      allVals = new Set()
+    }
+    allVals.add(value)
+    kv.set(key, allVals)
+  },
+  get: async function(key, value) {
+    
+  }
+}
 async function updateGameInfo() {
   
 }
