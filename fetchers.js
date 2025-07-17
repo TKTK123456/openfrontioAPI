bdec7f45f9a6e7767bb554859cde0eda8b33bff5
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-17 08:17:51 -0400

    Create fetchers.js

---
+++ fetchers.js
@@ -0,0 +1,22 @@
+
+async function findGameWebSocket(id, webSocketAmount = 20) {
+  for (let i = 0; i < webSocketAmount; i++) {
+    const response = await fetch(`https://blue.openfront.io/w${i}/api/game/${id}`);
+    if (response.status === 200) {
+      let output = await response.json()
+      return output
+    }
+  }
+}
+async function findPublicLobbyWebSocket(webSocketAmount = 20) {
+  let lobbies = await fetch(`https://blue.openfront.io/api/public_lobbies`)
+  lobbies = await lobbies.json()
+  lobbies = lobbies.lobbies
+  let output = []
+  lobbies.forEach((lobby) => {
+    output.push(findGameWebSocket(lobby.gameID, webSocketAmount))
+  });
+  output = await Promise.all(output)
+  return output
+}
+findPublicLobbyWebSocket().then(console.log)
