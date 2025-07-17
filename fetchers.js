904a05e17c88fd514b9dd9445bb0d45ea4eb10e2
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-17 11:51:01 -0400

    Update fetchers.js

--- fetchers.js
+++ fetchers.js
@@ -1,7 +1,5 @@
 import { setHelpers } from './info.js'
 const kv = await Deno.openKv();
-//kv.set(["info", "games", "active", "ids"], new Set())
-//kv.set(["info", "games", "active", "wsNum"], new Set())
 export async function findGameWebSocket(id, webSocketAmount = 20) {
   for (let i = 0; i < webSocketAmount; i++) {
     const response = await fetch(`https://blue.openfront.io/w${i}/api/game/${id}`);
