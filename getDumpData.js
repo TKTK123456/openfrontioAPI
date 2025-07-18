1f84971cc124250e98263ba2b89521eac2888b5f
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-17 21:39:57 -0400

    Update getDumpData.js

--- getDumpData.js
+++ getDumpData.js
@@ -1,3 +1,4 @@
+import { Octokit } from "@octokit/rest";
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN; // Set this in your environment securely
 const OWNER = 'TKTK123456';
 const REPO = 'openfrontioAPI';
@@ -5,24 +6,21 @@ const WORKFLOW_ID = 'extractOpenfrontData.yml'; // Workflow filename
 const REF = 'main'; // Branch name where workflow lives
 
 export default async function getDumpData(date) {
-  const url = `https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/dispatches`;
-  
-  const body = {
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN
+  })
+  let res = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+    owner: OWNER,
+    repo: REPO,
+    workflow_id: WORKFLOW_ID,
     ref: REF,
     inputs: {
-      date, // pass the date in YYYYMMDD format
+        date, // pass the date in YYYYMMDD format
     },
-  };
-
-  const res = await fetch(url, {
-    method: 'POST',
     headers: {
-      Authorization: `Bearer ${GITHUB_TOKEN}`,
-      Accept: 'application/vnd.github+json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  });
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
 
   if (res.status === 204) {
     console.log('Workflow triggered successfully!');
