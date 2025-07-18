4c90b17cd3d490496e1fb8f853e12c0572384ace
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-18 18:50:21 -0400

    Update fetchers.js

--- fetchers.js
+++ fetchers.js
@@ -5,7 +5,7 @@ const kv = await Deno.openKv();
 
 export async function findGameWebSocket(id, webSocketAmount = 20) {
   for (let i = 0; i < webSocketAmount; i++) {
-    const response = await fetch(`https://${config.prefix.use}${config.domain}/w${i}/api/game/${id}`);
+    const response = await fetch(`https://${config.prefixs.use}${config.domain}/w${i}/api/game/${id}`);
     if (response.status === 200) {
       setHelpers.add(["info", "games", "active", "ids"], id)
       mapHelpers.set(["info", "games", "active", "ws"], id, i)
@@ -14,7 +14,7 @@ export async function findGameWebSocket(id, webSocketAmount = 20) {
   }
 }
 export async function findPublicLobby(webSocketAmount = 20) {
-  let lobbies = await fetch(`https://${config.prefix.use}${config.domain}/api/public_lobbies`)
+  let lobbies = await fetch(`https://${config.prefixs.use}${config.domain}/api/public_lobbies`)
   lobbies = await lobbies.json()
   lobbies = lobbies.lobbies
   let output = new Map()
@@ -24,11 +24,11 @@ export async function findPublicLobby(webSocketAmount = 20) {
   return output
 }
 export async function getPlayer(id) {
-  let player = await fetch(`https://${config.prefix.api}${config.domain}/player/${id}`)
+  let player = await fetch(`https://${config.prefixs.api}${config.domain}/player/${id}`)
   return player
 }
 export async function getGame(id) {
-  let game = await fetch(`https://${config.prefix.api}${config.domain}/game/${id}`)
+  let game = await fetch(`https://${config.prefixs.api}${config.domain}/game/${id}`)
   if (game.status === 200) {
     setHelpers.add(["info", "games", "ids"], id)
   }
