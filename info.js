0e01d17c79a7a623a365083dfe1c0273d0460b1d
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-19 17:06:25 -0400

    Update info.js

--- info.js
+++ info.js
@@ -177,7 +177,7 @@ async function updateGameInfo(autoSetNextRun = true) {
     await saveFile(dateStr, existingArrays);
   }
   if (autoSetNextRun) {
-    console.log(publicLobbies[publicLobbies.length-1].msUntilStart-(startTime-Date.now())>0 ? publicLobbies[publicLobbies.length-1].msUntilStart-(startTime-Date.now()) : 500))
+    console.log(publicLobbies[publicLobbies.length-1].msUntilStart-(startTime-Date.now())>0 ? publicLobbies[publicLobbies.length-1].msUntilStart-(startTime-Date.now()) : 500)
     let nextTime = Math.min(getHuristicTime(), publicLobbies[publicLobbies.length-1].msUntilStart-(startTime-Date.now())>0 ? publicLobbies[publicLobbies.length-1].msUntilStart-(startTime-Date.now()) : 500)
     setTimeout(() => {
       console.log(`Setting next run for in ${nextTime}`)
