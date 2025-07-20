e56de6634d7186f7c8ff80a021b349e76ea29dc9
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-20 08:29:54 -0400

    Update info.js

--- info.js
+++ info.js
@@ -180,7 +180,7 @@ export async function updateGameInfo(autoSetNextRun = true) {
     await saveFile(dateStr, existingArrays);
   }
   let timeTaken = Date.now() - startTime
-  let lobbiesTimesToStart = publicLobbies.map(lobby => [lobby.msUntilStart,((lobby.gameConfig.maxPlayers-lobby.numClients)/1.75)*1000]).flat()
+  let lobbiesTimesToStart = publicLobbies.map(lobby => [lobby.msUntilStart,((lobby.gameConfig.maxPlayers-lobby.numClients)/3)*1000]).flat()
   lobbiesTimesToStart = lobbiesTimesToStart.map(time => (time-timeTaken>0 ? time-timeTaken : 500))
   let waitTime = Math.min(...lobbiesTimesToStart)
   updatingGameInfo = false
