f9d0f9ef9b617e89d3bb0635693fb1184bb700e9
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-27 19:36:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import express from 'express'
 import { WebSocketServer } from "ws";
 import path from 'node:path'
-import { setHelpers, mapHelpers, getGameIds, getAllGameIds, getRangeGameIds } from './info.js'
+import { setHelpers, mapHelpers, getGameIds, getAllGameIds, getRangeGameIds, getCordsFromTile } from './info.js'
 import { findGameWebSocket, findPublicLobby, getPlayer, getGame } from './fetchers.js'
 import config from './config.js'
 const __dirname = path.resolve();
