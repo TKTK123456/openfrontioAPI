4c0d4d5df753b3167d70ac0d8ab878f98c1322ef
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-07-29 11:18:16 -0400

    Update collectGameIds.js

--- collectGameIds.js
+++ collectGameIds.js
@@ -1,1 +1,14 @@
-
+import path from "node:path";
+const __dirname = path.resolve()
+async function readFile(filename) {
+  try {
+    const fullPath = path.join(__dirname, filename);
+    const text = await Deno.readTextFile(fullPath);
+    return text;
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      return null;
+    }
+    throw err;
+  }
+}
