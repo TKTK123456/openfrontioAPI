6e64a6c16eff302ffb299cf64185ddec428ec2cf
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-08-06 19:12:10 -0400

    Update info.js

--- info.js
+++ info.js
@@ -394,5 +394,5 @@ export async function updateGameInfo(autoSetNextRun = true, { type = "auto", log
     }
   }
 }
-await updateGameInfo(true, {type:"selfFetch"})
+await updateGameInfo(true, {type:"openfront.pro"})
 Deno.serve(() => new Response("selfFetch, world!"));
