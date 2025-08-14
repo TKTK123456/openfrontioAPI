264430b8608df5bc5b1b33850f29a97c8f3b1270
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-08-13 21:25:51 -0400

    Updating...

--- fetchers.js
+++ fetchers.js
@@ -1,53 +1,33 @@
-  import config from './config.js';
-import { remoteVars, remoteJsonStore } from './remoteVarStore.js';
-
-async function ensureActive() {
-  remoteVars.info = remoteVars.info ?? {};
-  remoteVars.info.games = remoteVars.info.games ?? {};
-  remoteVars.info.games.active = remoteVars.info.games.active ?? {};
-  remoteVars.info.games.active.ids = remoteVars.info.games.active.ids ?? new Set();
-  remoteVars.info.games.active.ws = remoteVars.info.games.active.ws ?? new Map();
-  await remoteJsonStore.save();
-}
+import { setHelpers, mapHelpers } from './info.js'
+import config from './config.js'
+const kv = await Deno.openKv();
+//kv.delete(["info", "games", "active", "ws"])
 
 export async function findGameWebSocket(id, webSocketAmount = 20) {
-  await ensureActive();
-  const activeIDs = remoteVars.info.games.active.ids;
-  const activeWS = remoteVars.info.games.active.ws;
-
   for (let i = 0; i < webSocketAmount; i++) {
     const response = await fetch(`https://${config.prefixs.use}${config.domain}/w${i}/api/game/${id}`);
     if (response.status === 200) {
-      activeIDs.add(id);
-      activeWS.set(id, i);
-      await remoteJsonStore.save();
-      return i;
+      setHelpers.add(["info", "games", "active", "ids"], id)
+      mapHelpers.set(["info", "games", "active", "ws"], id, i)
+      return i
     }
   }
-  return null;
 }
-
 export async function findPublicLobby(webSocketAmount = 20) {
-  let lobbies = await fetch(`https://${config.prefixs.use}${config.domain}/api/public_lobbies`);
-  lobbies = await lobbies.json();
-  lobbies = lobbies.lobbies;
-
-  let output = new Map();
-
+  let lobbies = await fetch(`https://${config.prefixs.use}${config.domain}/api/public_lobbies`)
+  lobbies = await lobbies.json()
+  lobbies = lobbies.lobbies
+  let output = new Map()
   await Promise.all(lobbies.map(async (lobby) => {
-    const ws = await findGameWebSocket(lobby.gameID, webSocketAmount);
-    output.set(lobby.gameID, { ...lobby, ws });
+    output.set(lobby.gameID, {...lobby, ws: await findGameWebSocket(lobby.gameID, webSocketAmount)})
   }));
-
-  return output;
+  return output
 }
-
 export async function getPlayer(id) {
-  const response = await fetch(`https://${config.prefixs.api}${config.domain}/player/${id}`);
-  return response.json();
+  let player = await fetch(`https://${config.prefixs.api}${config.domain}/player/${id}`)
+  return player
 }
-
 export async function getGame(id) {
-  const response = await fetch(`https://${config.prefixs.api}${config.domain}/game/${id}`);
-  return response;
+  let game = await fetch(`https://${config.prefixs.api}${config.domain}/game/${id}`)
+  return game
 }
