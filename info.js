512c5233f9d6e9f4f076d71987abc7be996bc4cc
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-08-14 02:56:47 -0400

    Updating...

--- info.js
+++ info.js
@@ -187,9 +187,6 @@ export async function getMapManifest(name) {
     return null;
   }
 }
-Deno.cron("Reminder to work", "*/3 * * * *", () => {
-  fetch("https://tktk123456-openfrontio-50.deno.dev/")
-});
 //Deno.cron("keep-bot-alive", "*/14 * * * *", async () => {
 //  const res = await fetch("https://openfrontapidiscordbots.onrender.com/ping");
 //  console.log("Pinged bot:", res.status);
@@ -201,5 +198,6 @@ Deno.cron("Fetch front plus game ids", "*/30 * * * *",async () => {
       remoteVars.active.ws.set(id, "unknown")
     })
     await remoteJsonStore.save()
+    fetch("https://tktk123456-openfrontio-50.deno.dev/")
   })
 });
